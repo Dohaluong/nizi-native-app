8da8eb87c6982c9f6dd4fcbe023daebf2e770abc
Do Ha Luong <dohaluong@gmail.com>
2026-07-24 12:59:07 +0700

    Initial Commit

---
+++ Nizi/NiziApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiziApp.swift
+//  Nizi
+//
+//  Created by Do Ha Luong on 7/24/26.
+//
+
+import SwiftUI
+
+@main
+struct NiziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NiziTests/NiziTests.swift
@@ -0,0 +1,17 @@
+//
+//  NiziTests.swift
+//  NiziTests
+//
+//  Created by Do Ha Luong on 7/24/26.
+//
+
+import Testing
+@testable import Nizi
+
+struct NiziTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
